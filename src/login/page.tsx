d009572b0b44c80d4a0d4aa5a2706cad064d0471
Mahrukh10 <sohailmahrukh864@gmail.com>
2026-02-01 14:17:32 +0500

    Login functionality implemented

--- src/login/page.tsx
+++ src/login/page.tsx
@@ -1,5 +1,38 @@
+"use client";
+import { useState } from "react";
 import Image from "next/image";
+
 export default function Login() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string>("");
+
+  const validateEmail = (value: string): void => {
+    const regex: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!regex.test(value)) {
+      setError("Please enter a valid email address");
+    } else {
+      setError("");
+    }
+  };
+  const handleSignIn = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (email == "test@test.com" && password == "password123")
+      console.log("Signing in with:", {
+        email,
+        password,
+      });
+    else console.log("wrong info");
+
+    // example:
+    // call API
+    // router.push("/dashboard")
+  };
+
+  const isFormValid = email !== "" && password !== "" && error === "";
+
   return (
     <div className="flex min-h-screen items-center justify-center">
       <main className="flex min-h-[80vh] w-full max-w-3xl flex-col items-center justify-center py-12 px-16 bg-white dark:bg-black sm:items-center rounded-sm m-16">
@@ -15,22 +48,49 @@ export default function Login() {
           <i>Get your favourites by your own choice</i>
         </p>
 
-        <form className="flex flex-col gap-4 w-full max-w-sm mt-4">
+        <form
+          onSubmit={handleSignIn}
+          className="flex flex-col gap-4 w-full max-w-sm mt-4"
+        >
           <input
             type="email"
             placeholder="Email"
-            className="px-4 py-2 border border-zinc-300 rounded-md focus:outline-none focus:ring-2 focus:ring-teal-400 dark:bg-zinc-800 dark:border-zinc-600 dark:text-zinc-50"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+              validateEmail(e.target.value);
+            }}
+            className={`px-4 py-2 border rounded-md
+            focus:outline-none focus:ring-2
+            ${
+              error
+                ? "border-red-500 focus:ring-red-400"
+                : "border-zinc-300 focus:ring-teal-400"
+            }
+            dark:bg-zinc-800 dark:border-zinc-600 dark:text-zinc-50`}
             required
           />
+
+          {error && <span className="text-sm text-red-500">{error}</span>}
           <input
             type="password"
             placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             className="px-4 py-2 border border-zinc-300 rounded-md focus:outline-none focus:ring-2 focus:ring-teal-400 dark:bg-zinc-800 dark:border-zinc-600 dark:text-zinc-50"
             required
           />
+
           <button
             type="submit"
-            className="bg-teal-400 text-white border font-semibold py-2 px-4 rounded-md hover:bg-teal-500 hover:border-black dark:hover:border-black transition-colors"
+            disabled={!isFormValid}
+            className={`font-semibold py-2 px-4 rounded-md border transition-colors
+              ${
+                isFormValid
+                  ? "bg-teal-400 text-white cursor-pointer hover:bg-teal-500 hover:border-black"
+                  : "bg-gray-300 text-gray-600 cursor-not-allowed"
+              }
+            `}
           >
             Sign In
           </button>
